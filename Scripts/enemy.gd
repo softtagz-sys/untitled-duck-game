a7c432e8692a438fc65518cd9e5d86ffdd51e3c3
Kobe Ponet <kobe.ponet@student.kdg.be>
2025-02-13 21:09:15 +0100

    added enemy movement

---
+++ Scripts/enemy.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+const SPEED : int = 50
+var playerEntered: bool = false
+var player: Node2D = null
+
+func _process(_delta: float) -> void:
+	if playerEntered and player:
+		var direction = (player.global_position - global_position).normalized()
+		velocity = direction * SPEED
+		move_and_slide()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):  # Ensure it's the player
+		playerEntered = true
+		player = body  # Store reference to the player
+		print("body entered")
+
+
+func _on_area_2d_body_exited(_body: Node2D) -> void:
+	playerEntered = false
