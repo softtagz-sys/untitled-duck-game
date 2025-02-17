0a936f5324e615e6191ae25355be0cf4b6910a54
Kobe Ponet <kobe.ponet@student.kdg.be>
2025-02-17 11:07:34 +0100

    refined character selection ui

--- character_creator.gd
+++ character_creator.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 var player_name: String = ""
 
-
-func _on_button_pressed() -> void:
+func _on_confirm_pressed() -> void:
 	Global.player_name = player_name
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
